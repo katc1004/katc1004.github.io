77ce44e0c2c95da25a2f920b85cf32068bb46b0e
Kathy <katc1004@users.noreply.github.com>
2020-06-21 01:50:11 -0500

    more formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
   	<Fragment>
   		<div id="about">
 		  	<div id="profile-pic"><img src={Propic} alt="Profile Pic"/></div>
-	  		Hello, my name is Kathy Chern. I am a Software Engineer.
+	  		<span>Hello, my name is Kathy Chern. I am a Software Engineer.</span>
 	  		<div id="links">
-	  			<a href="https://www.linkedin.com/in/kathychern95" target="blank"><AiFillLinkedin /></a>
-	  			<a href="https://github.com/katc1004" target="blank"><AiOutlineGithub /></a>
-	  			<a href={Resume} target="blank"><AiFillFile /></a>
-	  			<a href="mailto:kchern95@gmail.com"><AiFillMail /></a>
+	  			<a href="https://www.linkedin.com/in/kathychern95" target="blank"><AiFillLinkedin class="icon" size={24} /></a>
+	  			<a href="https://github.com/katc1004" target="blank"><AiOutlineGithub class="icon" size={24} /></a>
+	  			<a href={Resume} target="blank"><AiFillFile class="icon" size={24} /></a>
+	  			<a href="mailto:kchern95@gmail.com"><AiFillMail class="icon" size={24} /></a>
 	  		</div>
 		</div>
   	</Fragment>
